618aa50b39714e403d4c28728803758435a91f2e
Eclipse-V <christinevanson77@gmail.com>
2024-09-01 17:19:12 -0400

    Add files via upload

---
+++ partner.java
@@ -0,0 +1,26 @@
+/* Name: Eclipse Vanson         Title: Unit 0 Project              Date: 8/28/24 */
+// Description: For my portion of this assignment, I wanted to create a little bit of text art so I tried to incorporate my initals 
+// while I was making my little cat creature. The eyes of the cat were made with the first inital of my name "E". The inital of my 
+// middle name "N" were made to be the little stripes of the cat. The ears can little legs of the cat were made of the inital of my last name "V".
+
+public class Partner{
+    public static void main(String[] args){
+        System.out.println("                               J");
+        System.out.println("                              J J"); 
+        System.out.println("                             J J J"); 
+        System.out.println("                            J J J B"); 
+        System.out.println("                           J J J B B");
+        System.out.println("                          J J J B B B");
+        System.out.println("      ^-----^            J J J B B B S");  
+        System.out.println("     (E  --  E)         J J J B B B S S");
+        System.out.println("     /NNNNNNNN\\       J J J B B B S S S");
+        System.out.println("     /         \\       J J J B B B S S");
+        System.out.println("     (NNNNNNNNN)         J J J B B B S");
+        System.out.println("       V    V             J J J B B B");
+        System.out.println("                           J J J B B");
+        System.out.println("                            J J J B");
+        System.out.println("                             J J J");
+        System.out.println("                              J J");
+        System.out.println("                               J"); 
+    }
+}
