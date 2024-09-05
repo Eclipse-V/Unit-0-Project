26d5b5f0074ae5e892cb9774acdf25d26ddc877d
Eclipse-V <christinevanson77@gmail.com>
2024-09-04 20:31:44 -0400

    Update partner.java

--- partner.java
+++ partner.java
@@ -1,7 +1,5 @@
-/* Name: Eclipse Vanson         Title: Unit 0 Project              Date: 8/28/24 */
-// Description: For my portion of this assignment, I wanted to create a little bit of text art so I tried to incorporate my initals 
-// while I was making my little cat creature. The eyes of the cat were made with the first inital of my name "E". The inital of my 
-// middle name "N" were made to be the little stripes of the cat. The ears can little legs of the cat were made of the inital of my last name "V".
+/* Name: Eclipse Vanson and Jaskaran     Title: Unit 0 Project              Date: 8/28/24 */
+// Description: It's a cat that's looking at the diamond next to it. 
 
 public class Partner{
     public static void main(String[] args){
